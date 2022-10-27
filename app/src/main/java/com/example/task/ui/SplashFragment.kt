2075cea282c5ff054404a4ddb0b14b01f9ccc6af
Jefferson Douglas <86518315+Jeffitando@users.noreply.github.com>
2022-10-27 09:56:17 -0300

    Criação e configuração do Firebase

--- app/src/main/java/com/example/task/ui/SplashFragment.kt
+++ app/src/main/java/com/example/task/ui/SplashFragment.kt
@@ -1,23 +1,48 @@
 package com.example.task.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.task.R
+import com.example.task.databinding.SplashFragmentBinding
 
 
 class SplashFragment : Fragment() {
+
+    private var _binding: SplashFragmentBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+
+        _binding = SplashFragmentBinding.inflate(inflater, container, false)
+        return binding.root
 
     }
 
+    // As funcôes devem ser chamadas dentro do (OnViewCreated)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Tempo em que a Splash vai ficar visivel para o usuario
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 5000)
+    }
+
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/task/ui/auth/LoginFragment.kt
@@ -5,18 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.task.R
+import com.example.task.databinding.FragmentLoginBinding
+import com.example.task.databinding.SplashFragmentBinding
 
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    // As funcôes devem ser chamadas dentro do (OnViewCreated)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
