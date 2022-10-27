263045d8de4243a3b69afa458c1f5b3ad0ca8b3e
Jefferson Douglas <86518315+Jeffitando@users.noreply.github.com>
2022-10-26 22:57:49 -0300

    SplashScreen adicionada com a implementation Lottie

---
+++ app/src/main/java/com/example/task/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.task.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.task.R
+import com.example.task.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+    }
+
+}
