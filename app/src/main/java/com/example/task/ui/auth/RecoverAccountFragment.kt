7865248f93fec47675fbdedefa2dab135d657dab
Jefferson Douglas <86518315+Jeffitando@users.noreply.github.com>
2022-10-28 20:26:23 -0300

    preparando as fragments

--- app/src/main/java/com/example/task/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/example/task/ui/auth/RecoverAccountFragment.kt
@@ -6,18 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.task.R
+import com.example.task.databinding.FragmentRecoverAccountBinding
+import com.example.task.databinding.FragmentRegisterBinding
 
 
 class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recover_account, container, false)
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
+
+
--- app/src/main/java/com/example/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/task/ui/auth/RegisterFragment.kt
@@ -1,23 +1,63 @@
 package com.example.task.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.task.R
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.example.task.databinding.FragmentRegisterBinding
+import com.example.task.databinding.FragmentRegisterBinding.inflate
 
 
 class RegisterFragment : Fragment() {
 
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+
+                binding.progressBar.isVisible = true
+
+                registerUser(email, password)
+
+            } else {
+                Toast.makeText(requireContext(),"Informe sua senha.", LENGTH_SHORT).show()
+            }
+
+        } else {
+            Toast.makeText(requireContext(),"Informe seu e-mail.", LENGTH_SHORT).show()
+        }
+    }
+
+    private fun registerUser(email: String, password: String){
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
 }
+
+
+
+
+
