58050e3527e720d0acbdfcdb01d4ddc0acb6862d
Jefferson Douglas <86518315+Jeffitando@users.noreply.github.com>
2022-11-21 19:01:37 -0300

    Criado adapter de tarefas item do adapter task criado objeto task criado

---
+++ app/src/main/java/com/example/task/model/Task.kt
@@ -0,0 +1,17 @@
+package com.example.task.model
+
+import android.os.Parcelable
+import com.example.task.helper.FirebaseHelper
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Task(
+    var id: String = "",
+    var title: String = "",
+    var status: Int = 0
+) : Parcelable {
+    init {
+        //Para cada tarefa ter um Id unico no firebase
+        this.id = FirebaseHelper.getDatabase().push().key ?: ""
+    }
+}
